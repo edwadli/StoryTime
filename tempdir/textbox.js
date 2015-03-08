2ba24f851c2f3d4678ab48e0d555d521f6664e51
Edward Li <el2724@columbia.edu>
2015-03-08 00:35:43 -0500

    fixed conflicts in textbox.js

--- tempdir/textbox.js
+++ tempdir/textbox.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-$('.button_press').on('click', function(e) {
-	var str = $('.text_reader').val();
-	console.log(str);
-=======
 // make textbox interactive
 $('.button_press').on('click', function(e) { var str = $('.text_reader').val();
 console.log(str);
->>>>>>> origin/master
 });
 
