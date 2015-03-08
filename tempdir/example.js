1881b668d54da714fac8d9bf4555d8ef6edb3d68
Edward Li <el2724@columbia.edu>
2015-03-07 21:12:14 -0500

    editted server code

--- tempdir/example.js
+++ tempdir/example.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 
-fs.readFile('./sample.html',function(err,my_html){
-	if (err){
-		throw err;
-	}
+var after_fileread = function(err,my_html){
+                        if (err){
+                            throw err;
+                        }
 
+                        var my_server = http.createServer(function (req, res) {
+                            res.writeHead(200);
+                            res.write(my_html);
+                            res.end();
+                    }
+);
+                        my_server.listen(1337, '127.0.0.1');
 
-http.createServer(function (req, res) {
-  res.writeHead(200);
-  res.write(my_html);
-  res.end();
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+                        console.log('Server running at http://127.0.0.1:1337/');
+                    }
 
-
-});
+fs.readFile('./sample.html', after_fileread);
